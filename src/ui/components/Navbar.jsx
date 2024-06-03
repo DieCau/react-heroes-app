6fad9a3e136dd86305a93c272b07258a878c36df
Diego Caucota <diegocaucota@gmail.com>
2024-06-03 00:49:26 -0300

    add: helpers and components

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -38,18 +38,6 @@ export const Navbar = () => {
                         DC
                     </NavLink>
 
-                    <NavLink
-                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-                        to="/hero"
-                    >
-                        Hero
-                    </NavLink>
-                    <NavLink
-                        className={({ isActive }) => `nav-item nav-link ${isActive ? 'active' : ''}`}
-                        to="/search"
-                    >
-                        Search
-                    </NavLink>
                 </div>
             </div>
 
